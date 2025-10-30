c20b3f5fff62c5d6191ace21fb700948736cafed
Nickysukka72922 <Nicky.sukkar@gmail.com>
2025-10-30 13:22:10 +1100

    Delete src directory

--- src/app/search/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Suspense } from "react";
-
-export default function SearchLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return <Suspense>{children}</Suspense>;
-}
