46d800be4a2325fdb7dd42cfc114d4483e1515de
Nickysukka72922 <Nicky.sukkar@gmail.com>
2026-03-07 13:09:48 +1100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const cacheName = 'guess-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/icon-512.png'
+];
+
+// Install the service worker and cache the game code
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch the game from the cache if offline
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
