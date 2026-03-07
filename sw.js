ced4256d54fec06f4d8ea0a0f857bd181932cbdd
Nickysukka72922 <Nicky.sukkar@gmail.com>
2026-03-07 13:44:36 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,53 @@
-let deferredPrompt;
+// Versioning: Change this number whenever you update your game files
+const CACHE_NAME = 'guess-the-number-v21.7';
 
-// 1. Capture the event
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  // Don't hide the button yet! Just save the event.
-  document.getElementById('installBtn').style.display = 'block';
+// List every file you want to be available offline
+const ASSETS = [
+  '/',
+  './index.html',
+  './style.css',
+  './main.js',
+  './manifest.json',
+  './favicon.svg'
+  './hu.html',
+  './mainHu.js',
+  './manifestHu.json',
+  './website.png',
+  './styleHu.css',
+];
+
+// 1. Install: The Service Worker saves your files to the cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-// 2. You can now call this function from a menu or a link whenever you want!
-function triggerInstall() {
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
-    deferredPrompt = null;
-  } else {
-    alert("This app is already installed or your browser doesn't support it yet!");
-  }
-}
+// 2. Activate: Clean up old versions if you update the game
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Clearing old cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 3. Fetch: The "Brain" that serves files from the cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cached version or fetch from internet if not found
+      return response || fetch(event.request);
+    })
+  );
+});
