cdd820b9fcae0425f5f0d7e9c46c2cb7feaf1c40
Nickysukka72922 <Nicky.sukkar@gmail.com>
2026-03-08 18:01:13 +1100

    Update main.js

--- main.js
+++ main.js
@@ -16,3 +16,30 @@ function check(number){
 		//console.log('gess=',gess)
 	}
 }
+let deferredPrompt;
+const installBtn = document.getElementById('installBtn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // 1. Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // 2. Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // 3. Update UI notify the user they can install the PWA
+  installBtn.style.display = 'block';
+
+  installBtn.addEventListener('click', () => {
+    // 4. Hide the app provided install promotion
+    installBtn.style.display = 'none';
+    // 5. Show the install prompt
+    deferredPrompt.prompt();
+    // 6. Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
